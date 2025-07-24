bad1cde6bc6a800b91aab23c2c6d0b8d9add09d4
Ethan Maher <em157322@ohio.edu>
2025-07-24 19:12:06 -0400

    color test

--- frontend/src/components/ClothingList.tsx
+++ frontend/src/components/ClothingList.tsx
@@ -288,7 +288,7 @@ const ClothingList: React.FC = () => {
                     <td data-label="Size">{item.size}</td>
                     <td data-label="Price">${item.price.toFixed(2)}</td>
                     <td data-label="Color"><button
-                      className="pill-badge"
+                      className={`pill-badge ${(getColorHex(item.color) === '#ff69b4') ? 'force-hotpink' : ''}`}
                       style={{
                         backgroundColor: getColorHex(item.color),
                         color: getContrastColor(getColorHex(item.color)),
